b6835096834f202b08c27e5f4223ccf81a683893
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-03-21 10:20:37 +0530

    sorting algorithm

--- arrays.c
+++ arrays.c
@@ -201,3 +201,27 @@
 //     }
 //     return 0;
 // }
+int main(){
+    int i,j,arr[100],min,temp,n;
+    printf("Enter the no of array elements:\n");
+    scanf("%d",&n);
+    printf("Enter the array elements:\n");
+    for(i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+      for(i=0; i<n-1; i++){
+        min=i;
+        for(j=i+1; j<n; j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+    }
+    printf("The sorted array is:\n");
+for(i=0; i<n; i++){
+    printf("%d\n",arr[i]);
+}
+}
