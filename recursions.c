53b7f2b986598e15502e31f105fc3770156cb316
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-03-27 20:52:29 +0530

    Update recursions.c

--- recursions.c
+++ recursions.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 // for fibonnacci series
 // int fact(int n) {
 //     if (n == 0 || n == 1) {
@@ -13,44 +13,86 @@
 //     printf("Enter a number: ");
 //     scanf("%d", &n);
 //     int result = fact(n);
-//     printf("Factorial of %d is %d\n", n, result);   
+//     printf("Factorial of %d is %d\n", n, result);
 //     return 0;
 // }
 // Sum of digits
 #include <stdio.h>
-int sumofd(int n){
-    if (n==0){
+int sumofd(int n)
+{
+    if (n == 0)
+    {
         return 0;
     }
-    return (n%10)+sumofd(n/10);
-    
+    return (n % 10) + sumofd(n / 10);
 }
 
-int main() {
+int main()
+{
     int n;
     printf("enter your number:");
-    scanf("%d",&n);
-    int result=sumofd(n);
-    printf("The sum of digits is %d",result);
-    
-   
+    scanf("%d", &n);
+    int result = sumofd(n);
+    printf("The sum of digits is %d", result);
+
     return 0;
 }
-// printing nmber 1 to n 
+// printing nmber 1 to n
 #include <stdio.h>
-int printnos(int n){
-    if(n==0) return 0;
-    printnos(n-1);
-    printf("%d\n",n);
+int printnos(int n)
+{
+    if (n == 0)
+        return 0;
+    printnos(n - 1);
+    printf("%d\n", n);
 }
 
-int main() {
+int main()
+{
     int n;
     printf("enter your number:");
-    scanf("%d",&n);
-    int result=printnos(n);
-    printf("%d",result);
-    
-   
+    scanf("%d", &n);
+    int result = printnos(n);
+    printf("%d", result);
+
+    return 0;
+}
+//  Counting digits
+#include <stdio.h>
+
+int countDigits(int n)
+{
+    if (n == 0)
+        return 0;
+    return 1 + countDigits(n / 10);
+}
+
+int main()
+{
+    printf("Digits: %d", countDigits(98765));
+    return 0;
+}
+// reversing a number
+#include <stdio.h>
+
+int revhelper(int n, int rev)
+{
+    if (n == 0)
+        return rev;
+    int digit = n % 10;
+    rev = rev * 10 + digit;
+    return revhelper(n / 10, rev);
+}
+int revnum(int n)
+{
+    return revhelper(n, 0);
+}
+int main()
+{
+    int n;
+    printf("Enter your number");
+    scanf("%d", &n);
+    int result = revnum(n);
+    printf("The reverse number is:%d", result);
     return 0;
 }
