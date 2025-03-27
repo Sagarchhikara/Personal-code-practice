db8b356ac329b476c24c6ebe394b33585f70630f
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-03-27 21:01:42 +0530

    Update recursions.c

--- recursions.c
+++ recursions.c
@@ -96,3 +96,30 @@ int main()
     printf("The reverse number is:%d", result);
     return 0;
 }
+// checking if palindrome 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isPalindrome(char str[], int start, int end) {
+    if (start >= end)
+        return true;
+
+    if (str[start] != str[end])
+        return false;
+
+    return isPalindrome(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    if (isPalindrome(str, 0, strlen(str) - 1))
+        printf("Palindrome\n");
+    else
+        printf("Not a palindrome\n");
+
+    return 0;
+}
