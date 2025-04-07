9dfd8f1fd71460b6a4d51bff05e3a549e0e2c30c
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-04-07 15:00:14 +0530

    Update arrayANDpointer.c

--- arrayANDpointer.c
+++ arrayANDpointer.c
@@ -678,3 +678,18 @@
 // }
 // Transpose of matrix 
 // 
+int printfactorial(int n){
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+int main(){
+    int n;
+    printf("Enter a number: ");
+    int result = printfactorial(n);
+    printf("Factorial of %d is %d\n",n,result);
+    return 0;
+
+}
