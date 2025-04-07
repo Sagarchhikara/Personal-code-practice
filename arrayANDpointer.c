59bd28d1e9eff0bd1aac50e1751bfca36b56d9ed
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-04-07 14:58:12 +0530

    Update arrayANDpointer.c

--- arrayANDpointer.c
+++ arrayANDpointer.c
@@ -616,63 +616,65 @@
 //     return 0;
 // }
 // multilication of two matrices
-int main(){
-int mat1[10][10], mat2[10][10], result[10][10];
-int r1, c1, r2, c2;
-printf("Enter rows and columns for first matrix: ");
-scanf("%d%d", &r1, &c1);
+// int main(){
+// int mat1[10][10], mat2[10][10], result[10][10];
+// int r1, c1, r2, c2;
+// printf("Enter rows and columns for first matrix: ");
+// scanf("%d%d", &r1, &c1);
 
-printf("Enter rows and columns for second matrix: ");
-scanf("%d%d", &r2, &c2);
-if (c1 != r2)
-{
-    printf("Matrix multiplication not possible!\n");
-    return 0;
-}
-printf("Enter elements of first matrix:\n");
-for (int i = 0; i < r1; ++i)
-{
-    for (int j = 0; j < c1; ++j)
-    {
-        printf("mat1[%d][%d]: ", i, j);
-        scanf("%d", &mat1[i][j]);
-    }
-}
-printf("Enter elements of second matrix:\n");
-for (int i = 0; i < r2; ++i)
-{
-    for (int j = 0; j < c2; ++j)
-    {
-        printf("mat2[%d][%d]: ", i, j);
-        scanf("%d", &mat2[i][j]);
-    }
-}
-for (int i = 0; i < r1; ++i)
-{
-    for (int j = 0; j < c2; ++j)
-    {
-        result[i][j] = 0;
-    }
-}
-for (int i = 0; i < r1; ++i)
-{
-    for (int j = 0; j < c2; ++j)
-    {
-        for (int k = 0; k < c1; ++k)
-        {
-            result[i][j] += mat1[i][k] * mat2[k][j];
-        }
-    }
-}
-printf("Resultant matrix:\n");
-for (int i = 0; i < r1; ++i)
-{
-    for (int j = 0; j < c2; ++j)
-    {
-        printf("%d\t", result[i][j]);
-    }
-    printf("\n");
-}
+// printf("Enter rows and columns for second matrix: ");
+// scanf("%d%d", &r2, &c2);
+// if (c1 != r2)
+// {
+//     printf("Matrix multiplication not possible!\n");
+//     return 0;
+// }
+// printf("Enter elements of first matrix:\n");
+// for (int i = 0; i < r1; ++i)
+// {
+//     for (int j = 0; j < c1; ++j)
+//     {
+//         printf("mat1[%d][%d]: ", i, j);
+//         scanf("%d", &mat1[i][j]);
+//     }
+// }
+// printf("Enter elements of second matrix:\n");
+// for (int i = 0; i < r2; ++i)
+// {
+//     for (int j = 0; j < c2; ++j)
+//     {
+//         printf("mat2[%d][%d]: ", i, j);
+//         scanf("%d", &mat2[i][j]);
+//     }
+// }
+// for (int i = 0; i < r1; ++i)
+// {
+//     for (int j = 0; j < c2; ++j)
+//     {
+//         result[i][j] = 0;
+//     }
+// }
+// for (int i = 0; i < r1; ++i)
+// {
+//     for (int j = 0; j < c2; ++j)
+//     {
+//         for (int k = 0; k < c1; ++k)
+//         {
+//             result[i][j] += mat1[i][k] * mat2[k][j];
+//         }
+//     }
+// }
+// printf("Resultant matrix:\n");
+// for (int i = 0; i < r1; ++i)
+// {
+//     for (int j = 0; j < c2; ++j)
+//     {
+//         printf("%d\t", result[i][j]);
+//     }
+//     printf("\n");
+// }
 
-return 0;
-}
+// return 0;
+// }
+// Transpose of matrix 
+// 
