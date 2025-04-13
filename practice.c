758a65c69d5f1babebf622ddcb6009e19b28f7bb
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-04-13 11:06:43 +0530

    Update practice.c

--- practice.c
+++ practice.c
@@ -650,35 +650,35 @@
 
 //     return 0;
 // }
-#include <stdio.h>
-struct Distance {
-    int feet;
-    float inches;
-};
-int main() {
-    struct Distance d1, d2, sum;
+// #include <stdio.h>
+// struct Distance {
+//     int feet;
+//     float inches;
+// };
+// int main() {
+//     struct Distance d1, d2, sum;
 
-    printf("Enter first distance:\n");
-    printf("Feet: ");
-    scanf("%d", &d1.feet);
-    printf("Inches: ");
-    scanf("%f", &d1.inches);
+//     printf("Enter first distance:\n");
+//     printf("Feet: ");
+//     scanf("%d", &d1.feet);
+//     printf("Inches: ");
+//     scanf("%f", &d1.inches);
 
-    printf("\nEnter second distance:\n");
-    printf("Feet: ");
-    scanf("%d", &d2.feet);
-    printf("Inches: ");
-    scanf("%f", &d2.inches);
+//     printf("\nEnter second distance:\n");
+//     printf("Feet: ");
+//     scanf("%d", &d2.feet);
+//     printf("Inches: ");
+//     scanf("%f", &d2.inches);
 
-    sum.feet = d1.feet + d2.feet;
-    sum.inches = d1.inches + d2.inches;
+//     sum.feet = d1.feet + d2.feet;
+//     sum.inches = d1.inches + d2.inches;
 
-    if (sum.inches >= 12.0) {
-        sum.inches -= 12.0;
-        sum.feet++;
-    }
-    printf("\nTotal distance = %d feet %.2f inches\n", sum.feet, sum.inches);
+//     if (sum.inches >= 12.0) {
+//         sum.inches -= 12.0;
+//         sum.feet++;
+//     }
+//     printf("\nTotal distance = %d feet %.2f inches\n", sum.feet, sum.inches);
 
-    return 0;
-}
+//     return 0;
+// }
 
