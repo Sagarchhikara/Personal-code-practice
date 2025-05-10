7646b54df6909be6978d92314ba42bbe402d170d
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-05-10 14:12:08 +0530

    Update tutorial1.c

--- tutorial1.c
+++ tutorial1.c
@@ -7,6 +7,7 @@ int main(){
     printf(" The adress of 2nd element of array is %d\n",arr+1);
 
     printf(" Value at position 3 pf array is %d\n",*(&arr[0]));
+    printf(" Value at position 3 pf array is %d\n",*(&arr[3]));
     printf(" The value at adress of 1st element of array is %d\n",*(&arr[0]));
     printf(" The value at adress of 2nd element of array is %d\n",*(&arr[1]));
     printf(" The value at adress of 2nd element of array is %d\n",*(arr+1));
