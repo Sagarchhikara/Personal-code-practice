636d455ce3132dd118e22c81712af689d96b03f5
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-05-14 14:56:33 +0530

    Update examprac.c

--- examprac.c
+++ examprac.c
@@ -2,10 +2,21 @@
 #include<string.h>
 int main(){
     char str[100];
-    printf("Enter a string: \n");
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0'; // Remove the trailing newline character
-    int length=strlen(str);
-    printf("%d",length);
+    int len = 0;
+    printf("Enter a sting:\n");
+    scanf("%s",str);
+    while (str[len]!=0)
+    {
+        /* code */
+        len++;
+    }
+    for(int i=0; i<len; i++){
+        char temp=str[i];
+        str[i]=str[len-i-1];
+        str[len-i-1]=temp;
+
+
+    }
+    printf("Reversed string is %s",str);
     return 0;
-}
+}   
