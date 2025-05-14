f6e8b0fd1c35ef36862a3e094600c3a6bd12523e
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-05-14 15:10:00 +0530

    Update examprac.c

--- examprac.c
+++ examprac.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
 #include<string.h>
-int main(){
-    char str[100]; 
+
+int main() {
+    char str[100];
+    
     printf("Enter a String: \n");
-    scanf("%s",str);
-    gets(str);
-    int length=0;
-    char vowels,consonants;
-    while (str[length]!='0\n')
-    {
+    scanf("%99s", str); // Limit input to avoid buffer overflow
+    getchar(); // Consume the leftover newline character
+    
+    int length = 0;
+    int vowels = 0, consonants = 0; // Initialize counters
+    
+    while (str[length] != '\0') {
         length++;
     }
     
-    for (int  i = 0; i < length; i++)
-    {
-        if(str[i]=='a','e','i','o','u','A','E','I','O','U'){
+    for (int i = 0; i < length; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
             vowels++;
-        }
-        else {
+        } else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
             consonants++;
         }
     }
-    printf("Number of vowels: %d\n",vowels);
-    printf("Number of consonants: %d\n",consonants);
+    
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+    
     return 0;
-}   
+}
