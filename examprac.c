cd95a1e5fff3075d18d05743ebe57eff6dc510e3
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-05-14 14:46:19 +0530

    Update examprac.c

--- examprac.c
+++ examprac.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
 #include<string.h>
+int main(){
+    char str[100];
+    printf("Enter a string: \n");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Remove the trailing newline character
+    int length=strlen(str);
+    printf("%d",length);
+    return 0;
+}
