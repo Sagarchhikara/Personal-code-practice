b08d22870f54b33261291a9f4efba06a9f154f14
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-05-28 18:18:26 +0530

    Update fivemarks.c

--- fivemarks.c
+++ fivemarks.c
@@ -139,3 +139,25 @@ int main()
 
     return 0;
 }
+// Online C compiler to run C program online
+#include <stdio.h>
+int gcd(int a,int b);
+int lcm(int a,int b);
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    
+    printf("%d",lcm(a,b));
+    return 0;
+    
+}
+int gcd(int a, int b) { 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b); 
+} 
+ 
+// Function to compute LCM 
+int lcm(int a, int b) { 
+    return (a / gcd(a, b)) * b; 
+}
