ef3087393b13772b8723b16ae05952c410a439b9
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-05-29 10:21:40 +0530

    Update fivemarks.c

--- fivemarks.c
+++ fivemarks.c
@@ -226,24 +226,24 @@
 
 //     return 0;
 // }
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    if(n<5||n>20){
-        printf("Invalid input");
-        return 0;
-    }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            if(i==1||j==1||j==n||i==n){
-                printf("*");
-            }
-            else{
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     if(n<5||n>20){
+//         printf("Invalid input");
+//         return 0;
+//     }
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             if(i==1||j==1||j==n||i==n){
+//                 printf("*");
+//             }
+//             else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
