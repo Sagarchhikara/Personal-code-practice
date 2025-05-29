67e384bb9643348fad3347f4d50a7ea2ca8153a1
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-05-29 10:26:28 +0530

    Update fivemarks.c

--- fivemarks.c
+++ fivemarks.c
@@ -247,20 +247,20 @@
 //     }
 //     return 0;
 // }
-#include<stdio.h>
-int countways(int n){
-    if(n==0) return 1;
-    if(n<0) return 0;
-    return countways(n-1)+countways(n-2);
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    if(n<=0||n>30){
-        printf("Invalid Input!");
-    }
-    else{
-        printf("%d\n",countways(n));
-    }
-    return 0;
-}
+// #include<stdio.h>
+// int countways(int n){
+//     if(n==0) return 1;
+//     if(n<0) return 0;
+//     return countways(n-1)+countways(n-2);
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     if(n<=0||n>30){
+//         printf("Invalid Input!");
+//     }
+//     else{
+//         printf("%d\n",countways(n));
+//     }
+//     return 0;
+// }
