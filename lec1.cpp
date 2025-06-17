d912e33ee2e736a3325832237c8a66c09dddf402
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-06-17 22:08:52 +0530

    Update lec1.cpp

--- lec1.cpp
+++ lec1.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    char grade='A';
-    int value=grade;
-    cout<<value<<endl;;
+    double price=100.99;
+    int newprice=(int)price;
+
+    cout<<price<<endl;
+    cout<<newprice<<endl;
     return 0;
 }
