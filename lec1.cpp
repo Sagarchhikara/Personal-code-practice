af02f992f01c102a64ef5824beb259919aa3ac7c
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-07-03 09:15:53 +0530

    Update lec1.cpp

--- lec1.cpp
+++ lec1.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello, World!" << endl;
-    cout << "This is a C++14 program." << endl;
-    return 0;
-}
+// int main()
+// {
+//     cout << "Hello, World!" << endl;
+//     cout << "This is a C++14 program." << endl;
+//     return 0;
+// }
+// int main()
+// {
+//     int n = 5;
+//     int arr[4] = {5, 1, 3, 2};
+//     int missingNumber = 0;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         missingNumber ^= i;
+//     }
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         missingNumber ^= arr[i];
+//     }
+//     cout << missingNumber;
+// }
+// int main(){
+//     int n=78;
+//     if (n==0) return -1;
+//     int ans=1;
+//     while (n%2==0){
+//         n=n>>1;
+//         ans++;
+//     }
+//     cout<<ans;
+
+// }
