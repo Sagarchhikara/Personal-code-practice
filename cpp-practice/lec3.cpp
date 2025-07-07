65ac1d04db4615d365684ab8b901098f7cb732c8
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-07 10:10:00 +0530

    Update lec3.cpp

---
+++ cpp-practice/lec3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    char vowel = 'a';
+    if ((vowel == 'a') || (vowel = 'e') || (vowel = 'i'))
+    {
+        cout << " vowel";
+    }
+    else
+    {
+        cout << "constants";
+    }
+
+    return 0;
+}
