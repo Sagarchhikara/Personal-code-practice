7061905d5e9ce3f3d0d2f0e6226565b697cc6f1e
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-07 10:44:56 +0530

    Update lec3.cpp

--- cpp-practice/lec3.cpp
+++ cpp-practice/lec3.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char const *argv[])
-{
-    char vowel = 'a';
-    if ((vowel == 'a') || (vowel = 'e') || (vowel = 'i'))
-    {
-        cout << " vowel";
-    }
-    else
-    {
-        cout << "constants";
-    }
+// {
+//     char vowel = 'a';
+//     if ((vowel == 'a') || (vowel = 'e') || (vowel = 'i'))
+//     {
+//         cout << " vowel";
+//     }
+//     else
+//     {
+//         cout << "constants";
+//     }
 
-    return 0;
+//     return 0;
+// }
+{
 }
