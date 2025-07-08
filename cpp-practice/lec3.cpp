2206391b9d308abc6dcf853299e52a4aa4b2304f
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-08 10:09:38 +0530

    Update lec3.cpp

--- cpp-practice/lec3.cpp
+++ cpp-practice/lec3.cpp
@@ -1,57 +1,67 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char const *argv[])
-    // {
-    //     char vowel = 'a';
-    //     if ((vowel == 'a') || (vowel = 'e') || (vowel = 'i'))
-    //     {
-    //         cout << " vowel";
-    //     }
-    //     else
-    //     {
-    //         cout << "constants";
-    //     }
+// {
+//     char vowel = 'a';
+//     if ((vowel == 'a') || (vowel = 'e') || (vowel = 'i'))
+//     {
+//         cout << " vowel";
+//     }
+//     else
+//     {
+//         cout << "constants";
+//     }
 
-    //     return 0;
-    // }
-    // {
-    //     int a = 3, b = 5;
-    //     int sum = a + b;
-    //     cout << sum;
-    //     return 0;
-    // }
-    // wap to accept a number for weekday and saplay accordingly
-    // {
-    //     int n;
-    //     cout << "Enter a number for a day:\n";
-    //     cin >> n;
-    //     switch (n)
-    //     {
-    //     case 1:
-    //         cout << "Monday";
-    //         break;
-    //     case 2:
-    //         cout << "Tuesday";
-    //         break;
-    //     case 3:
-    //         cout << "Wednesday";
-    //         break;
-    //     case 4:
-    //         cout << "Thursday";
-    //         break;
-    //     case 5:
-    //         cout << "Friday";
-    //         break;
-    //     case 6:
-    //         cout << "Saturday";
-    //         break;
-    //     case 7:
-    //         cout << "Sunday";
-    //         break;
+//     return 0;
+// }
+// {
+//     int a = 3, b = 5;
+//     int sum = a + b;
+//     cout << sum;
+//     return 0;
+// }
+// wap to accept a number for weekday and saplay accordingly
+// {
+//     int n;
+//     cout << "Enter a number for a day:\n";
+//     cin >> n;
+//     switch (n)
+//     {
+//     case 1:
+//         cout << "Monday";
+//         break;
+//     case 2:
+//         cout << "Tuesday";
+//         break;
+//     case 3:
+//         cout << "Wednesday";
+//         break;
+//     case 4:
+//         cout << "Thursday";
+//         break;
+//     case 5:
+//         cout << "Friday";
+//         break;
+//     case 6:
+//         cout << "Saturday";
+//         break;
+//     case 7:
+//         cout << "Sunday";
+//         break;
 
-    //     default:
-    //         cout << "Enter a valid number from 1-7";
-    //         break;
-    //     }
-    //     return 0;
-    // }
+//     default:
+//         cout << "Enter a valid number from 1-7";
+//         break;
+//     }
+//     return 0;
+// }
+//   LOOPS
+
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
