ea00056829d9ff4edd9da49c40d73aefde1ceb77
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-08 09:40:16 +0530

    Update lec3.cpp

--- cpp-practice/lec3.cpp
+++ cpp-practice/lec3.cpp
@@ -14,5 +14,44 @@ int main(int argc, char const *argv[])
 
 //     return 0;
 // }
+// {
+//     int a = 3, b = 5;
+//     int sum = a + b;
+//     cout << sum;
+//     return 0;
+// }
+// wap to accept a number for weekday and saplay accordingly
 {
+    int n;
+    cout << "Enter a number for a day:\n";
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+
+    default:
+        cout << "Enter a valid number from 1-7";
+        break;
+    }
+    return 0;
 }
