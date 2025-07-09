1cfbd891a194637bbbfaa2668040241afe7c7dbd
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-09 10:20:49 +0530

    changes ,ade

--- cpp-practice/lec4.cpp
+++ cpp-practice/lec4.cpp
@@ -2,78 +2,78 @@
 #include <iostream>
 using namespace std;
 int main()
-// {
-//     int i, j, n;
-//     cout << "Enter the number of rows" << endl;
-//     cin >> n;
-//     for (i = 0; i <= n; i++)
-//     {
-//         for (j = 0; j <= n; j++)
-//         {
-//             cout << "* ";
-//         }
-//         cout << "\n";
-//     }
-//     return 0;
-// }
-// {
-//     int n;
-//     cout << "Enter the number:" << endl;
-//     cin >> n;
-//     for (int i = n; i >= 0; i--)
-//     {
-//         for (int j = 1; j <= i; j++)
-//         {
-//             cout << "* ";
-//         }
-//         cout << "\n";
-//     }
-//     return 0;
-// }
-// {
-//     for (int i = 0; i <= 5; i++)
-//     {
-//         for (int j = 0; j <= 5; j++)
-//         {
-//             cout << "* ";
-//         }
-//         cout << endl;
-//     }
-//     return 0;
-// }
-// {
-//     int i, j, s, sp = 4;
-//     for (int i = 1; i <= 5; i++)
-//     {
-//         for (s = sp; s > 0; s--)
-//         {
-//             cout << " ";
-//         }
-//         for (j = i; j > 0; j--)
-//         {
-//             cout << "*";
-//         }
-//         sp--;
-//         cout << endl;
-//     }
-//     return 0;
-// }
-{
-    int n;
-    cout << "ENter the nuber:\n";
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n - i; j++)
-        {
-            cout << " ";
-        }
-        for (int k = 1; k <= 2 * i - 1; k++)
-        {
-            cout << "*";
-        }
-        cout << "\n";
-    }
+    // {
+    //     int i, j, n;
+    //     cout << "Enter the number of rows" << endl;
+    //     cin >> n;
+    //     for (i = 0; i <= n; i++)
+    //     {
+    //         for (j = 0; j <= n; j++)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         cout << "\n";
+    //     }
+    //     return 0;
+    // }
+    // {
+    //     int n;
+    //     cout << "Enter the number:" << endl;
+    //     cin >> n;
+    //     for (int i = n; i >= 0; i--)
+    //     {
+    //         for (int j = 1; j <= i; j++)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         cout << "\n";
+    //     }
+    //     return 0;
+    // }
+    // {
+    //     for (int i = 0; i <= 5; i++)
+    //     {
+    //         for (int j = 0; j <= 5; j++)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         cout << endl;
+    //     }
+    //     return 0;
+    // }
+    // {
+    //     int i, j, s, sp = 4;
+    //     for (int i = 1; i <= 5; i++)
+    //     {
+    //         for (s = sp; s > 0; s--)
+    //         {
+    //             cout << " ";
+    //         }
+    //         for (j = i; j > 0; j--)
+    //         {
+    //             cout << "*";
+    //         }
+    //         sp--;
+    //         cout << endl;
+    //     }
+    //     return 0;
+    // }
+    // {
+    //     int n;
+    //     cout << "ENter the nuber:\n";
+    //     cin >> n;
+    //     for (int i = 1; i <= n; i++)
+    //     {
+    //         for (int j = 1; j <= n - i; j++)
+    //         {
+    //             cout << " ";
+    //         }
+    //         for (int k = 1; k <= 2 * i - 1; k++)
+    //         {
+    //             cout << "*";
+    //         }
+    //         cout << "\n";
+    //     }
 
-    return 0;
-}
+    //     return 0;
+    // }
