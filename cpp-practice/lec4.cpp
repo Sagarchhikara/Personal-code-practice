ddd5a70d6b2b9f152965a55506f90197a7414f7b
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-09 09:30:45 +0530

    Update lec4.cpp

--- cpp-practice/lec4.cpp
+++ cpp-practice/lec4.cpp
@@ -2,17 +2,31 @@
 #include <iostream>
 using namespace std;
 int main()
-    // {
-    //     int i, j, n;
-    //     cout << "Enter the number of rows" << endl;
-    //     cin >> n;
-    //     for (i = 0; i <= n; i++)
-    //     {
-    //         for (j = 0; j <= n; j++)
-    //         {
-    //             cout << "* ";
-    //         }
-    //         cout << "\n";
-    //     }
-    //     return 0;
-    // }
+// {
+//     int i, j, n;
+//     cout << "Enter the number of rows" << endl;
+//     cin >> n;
+//     for (i = 0; i <= n; i++)
+//     {
+//         for (j = 0; j <= n; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+{
+    int n;
+    cout << "Enter the number:" << endl;
+    cin >> n;
+    for (int i = n; i >= 0; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
