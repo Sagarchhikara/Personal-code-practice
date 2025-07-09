45c041c3fce985dda790d5c2f8649e9be7495bbf
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-09 09:15:38 +0530

    Update lec3.cpp

--- cpp-practice/lec3.cpp
+++ cpp-practice/lec3.cpp
@@ -1,132 +1,160 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char const *argv[])
-// {
-//     char vowel = 'a';
-//     if ((vowel == 'a') || (vowel = 'e') || (vowel = 'i'))
-//     {
-//         cout << " vowel";
-//     }
-//     else
-//     {
-//         cout << "constants";
-//     }
+    // {
+    //     char vowel = 'a';
+    //     if ((vowel == 'a') || (vowel = 'e') || (vowel = 'i'))
+    //     {
+    //         cout << " vowel";
+    //     }
+    //     else
+    //     {
+    //         cout << "constants";
+    //     }
 
-//     return 0;
-// }
-// {
-//     int a = 3, b = 5;
-//     int sum = a + b;
-//     cout << sum;
-//     return 0;
-// }
-// wap to accept a number for weekday and saplay accordingly
-// {
-//     int n;
-//     cout << "Enter a number for a day:\n";
-//     cin >> n;
-//     switch (n)
-//     {
-//     case 1:
-//         cout << "Monday";
-//         break;
-//     case 2:
-//         cout << "Tuesday";
-//         break;
-//     case 3:
-//         cout << "Wednesday";
-//         break;
-//     case 4:
-//         cout << "Thursday";
-//         break;
-//     case 5:
-//         cout << "Friday";
-//         break;
-//     case 6:
-//         cout << "Saturday";
-//         break;
-//     case 7:
-//         cout << "Sunday";
-//         break;
+    //     return 0;
+    // }
+    // {
+    //     int a = 3, b = 5;
+    //     int sum = a + b;
+    //     cout << sum;
+    //     return 0;
+    // }
+    // wap to accept a number for weekday and saplay accordingly
+    // {
+    //     int n;
+    //     cout << "Enter a number for a day:\n";
+    //     cin >> n;
+    //     switch (n)
+    //     {
+    //     case 1:
+    //         cout << "Monday";
+    //         break;
+    //     case 2:
+    //         cout << "Tuesday";
+    //         break;
+    //     case 3:
+    //         cout << "Wednesday";
+    //         break;
+    //     case 4:
+    //         cout << "Thursday";
+    //         break;
+    //     case 5:
+    //         cout << "Friday";
+    //         break;
+    //     case 6:
+    //         cout << "Saturday";
+    //         break;
+    //     case 7:
+    //         cout << "Sunday";
+    //         break;
 
-//     default:
-//         cout << "Enter a valid number from 1-7";
-//         break;
-//     }
-//     return 0;
-// }
-//   LOOPS
-// {
-//     // For loop - Ascending
-//     cout << "For loop ascending: ";
-//     for (int i = 1; i <= 10; i++)
-//     {
-//         cout << i << " ";
-//     }
-//     cout << endl;
+    //     default:
+    //         cout << "Enter a valid number from 1-7";
+    //         break;
+    //     }
+    //     return 0;
+    // }
+    //   LOOPS
+    // {
+    //     // For loop - Ascending
+    //     cout << "For loop ascending: ";
+    //     for (int i = 1; i <= 10; i++)
+    //     {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
 
-//     // For loop - Descending
-//     cout << "For loop descending: ";
-//     for (int i = 10; i >= 1; i--)
-//     {
-//         cout << i << " ";
-//     }
-//     cout << endl;
-//     // While loop - Ascending
-//     cout
-//         << "While loop ascending: ";
-//     int i = 1;
-//     while (i <= 10)
-//     {
-//         cout << i << " ";
-//         i++;
-//     }
-//     cout << endl;
+    //     // For loop - Descending
+    //     cout << "For loop descending: ";
+    //     for (int i = 10; i >= 1; i--)
+    //     {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    //     // While loop - Ascending
+    //     cout
+    //         << "While loop ascending: ";
+    //     int i = 1;
+    //     while (i <= 10)
+    //     {
+    //         cout << i << " ";
+    //         i++;
+    //     }
+    //     cout << endl;
 
-//     // While loop - Descending
-//     cout << "While loop descending: ";
-//     i = 10;
-//     while (i >= 1)
-//     {
-//         cout << i << " ";
-//         i--;
-//     }
-//     cout << endl;
+    //     // While loop - Descending
+    //     cout << "While loop descending: ";
+    //     i = 10;
+    //     while (i >= 1)
+    //     {
+    //         cout << i << " ";
+    //         i--;
+    //     }
+    //     cout << endl;
 
-//     // Do while loop - Ascending
-//     cout << "Do while loop ascending: ";
-//     i = 1;
-//     do
-//     {
-//         cout << i << " ";
-//         i++;
-//     } while (i <= 10);
-//     cout << endl;
+    //     // Do while loop - Ascending
+    //     cout << "Do while loop ascending: ";
+    //     i = 1;
+    //     do
+    //     {
+    //         cout << i << " ";
+    //         i++;
+    //     } while (i <= 10);
+    //     cout << endl;
 
-//     // Do while loop - Descending
-//     cout << "Do while loop descending: ";
-//     i = 10;
-//     do
-//     {
-//         cout << i << " ";
-//         i--;
-//     } while (i >= 1);
-//     cout << endl;
+    //     // Do while loop - Descending
+    //     cout << "Do while loop descending: ";
+    //     i = 10;
+    //     do
+    //     {
+    //         cout << i << " ";
+    //         i--;
+    //     } while (i >= 1);
+    //     cout << endl;
 
-//     return 0;
-// }
-{
-    int i;
-    for (i = 25; i >= 10; i--)
-    {
-        if (i % 2 == 0)
-        {
-            cout << "even number: " << i << endl;
-        }
-        else
-        {
-            cout << "odd number: " << i << endl;
-        }
-    }
-    return 0;
-}
+    //     return 0;
+    // }
+    // {
+    //     int i;
+    //     for (i = 25; i >= 10; i--)
+    //     {
+    //         if (i % 2 == 0)
+    //         {
+    //             cout << "even number: " << i << endl;
+    //         }
+    //         else
+    //         {
+    //             cout << "odd number: " << i << endl;
+    //         }
+    //     }
+    //     return 0;
+    // }
+
+    //     int i=1;
+    //     do{
+    //         cout<<i<<endl;
+    //         i++;
+    //     }while(i<=10);
+    // }
+
+    // {
+    //     int i;
+    //     for (i = 25; i >= 10; i--)
+    //     {
+    //         if (i % 2 != 0)
+    //             cout << i << endl;
+    //         continue;
+    //     }
+    // }
+
+    // {
+    //     int i, j;
+    //     int n = 1;
+    //     for (i = 1; i <= 3; i++)
+    //     {
+    //         for (j = 1; j <= 3; j++)
+    //             cout << n << " ";
+    //         cout << endl;
+    //     }
+    // }
