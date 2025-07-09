160f169375e1174541d74c340314a6c1bc7e76be
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-09 10:17:46 +0530

    Update lec4.cpp

--- cpp-practice/lec4.cpp
+++ cpp-practice/lec4.cpp
@@ -41,20 +41,39 @@ int main()
 //     }
 //     return 0;
 // }
+// {
+//     int i, j, s, sp = 4;
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         for (s = sp; s > 0; s--)
+//         {
+//             cout << " ";
+//         }
+//         for (j = i; j > 0; j--)
+//         {
+//             cout << "*";
+//         }
+//         sp--;
+//         cout << endl;
+//     }
+//     return 0;
+// }
 {
-    int i, j, s, sp = 4;
-    for (int i = 1; i <= 5; i++)
+    int n;
+    cout << "ENter the nuber:\n";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
     {
-        for (s = sp; s > 0; s--)
+        for (int j = 1; j <= n - i; j++)
         {
             cout << " ";
         }
-        for (j = i; j > 0; j--)
+        for (int k = 1; k <= 2 * i - 1; k++)
         {
             cout << "*";
         }
-        sp--;
-        cout << endl;
+        cout << "\n";
     }
+
     return 0;
 }
