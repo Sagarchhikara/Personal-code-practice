24f0c0c821c641d340d35851004e7eaa7b2be91e
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-11 10:54:11 +0530

    Update lec4.cpp

--- cpp-practice/lec4.cpp
+++ cpp-practice/lec4.cpp
@@ -2,190 +2,197 @@
 #include <iostream>
 using namespace std;
 int main()
-    // {
-    //     int i, j, n;
-    //     cout << "Enter the number of rows" << endl;
-    //     cin >> n;
-    //     for (i = 0; i <= n; i++)
-    //     {
-    //         for (j = 0; j <= n; j++)
-    //         {
-    //             cout << "* ";
-    //         }
-    //         cout << "\n";
-    //     }
-    //     return 0;
-    // }
-    // {
-    //     int n;
-    //     cout << "Enter the number:" << endl;
-    //     cin >> n;
-    //     for (int i = n; i >= 0; i--)
-    //     {
-    //         for (int j = 1; j <= i; j++)
-    //         {
-    //             cout << "* ";
-    //         }
-    //         cout << "\n";
-    //     }
-    //     return 0;
-    // }
-    // {
-    //     for (int i = 0; i <= 5; i++)
-    //     {
-    //         for (int j = 0; j <= 5; j++)
-    //         {
-    //             cout << "* ";
-    //         }
-    //         cout << endl;
-    //     }
-    //     return 0;
-    // }
-    // {
-    //     int i, j, s, sp = 4;
-    //     for (int i = 1; i <= 5; i++)
-    //     {
-    //         for (s = sp; s > 0; s--)
-    //         {
-    //             cout << " ";
-    //         }
-    //         for (j = i; j > 0; j--)
-    //         {
-    //             cout << "*";
-    //         }
-    //         sp--;
-    //         cout << endl;
-    //     }
-    //     return 0;
-    // }
-    // {
-    //     int n;
-    //     cout << "ENter the nuber:\n";
-    //     cin >> n;
-    //     for (int i = 1; i <= n; i++)
-    //     {
-    //         for (int j = 1; j <= n - i; j++)
-    //         {
-    //             cout << " ";
-    //         }
-    //         for (int k = 1; k <= 2 * i - 1; k++)
-    //         {
-    //             cout << "*";
-    //         }
-    //         cout << "\n";
-    //     }
+// {
+//     int i, j, n;
+//     cout << "Enter the number of rows" << endl;
+//     cin >> n;
+//     for (i = 0; i <= n; i++)
+//     {
+//         for (j = 0; j <= n; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+// {
+//     int n;
+//     cout << "Enter the number:" << endl;
+//     cin >> n;
+//     for (int i = n; i >= 0; i--)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+// {
+//     for (int i = 0; i <= 5; i++)
+//     {
+//         for (int j = 0; j <= 5; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// {
+//     int i, j, s, sp = 4;
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         for (s = sp; s > 0; s--)
+//         {
+//             cout << " ";
+//         }
+//         for (j = i; j > 0; j--)
+//         {
+//             cout << "*";
+//         }
+//         sp--;
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// {
+//     int n;
+//     cout << "ENter the nuber:\n";
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int k = 1; k <= 2 * i - 1; k++)
+//         {
+//             cout << "*";
+//         }
+//         cout << "\n";
+//     }
 
-    //     return 0;
-    // }
-    // #include <stdio.h>
+//     return 0;
+// }
+// #include <stdio.h>
 
-    // {
-    //     int n, i, j;
-    //     cin >> n;
-    //     for (i = 1; i <= n; i++)
-    //     {
-    //         for (j = i; j < n; j++)
-    //         {
-    //             cout << " ";
-    //         }
-    //         for (j = 1; j <= (2 * i - 1); j++)
-    //         {
-    //             cout << "*";
-    //         }
-    //         cout << "\n";
-    //     }
-    //     for (i = n - 1; i >= 1; i--)
-    //     {
-    //         for (j = n; j > i; j--)
-    //         {
-    //             cout << " ";
-    //         }
-    //         for (j = 1; j <= (2 * i - 1); j++)
-    //         {
-    //             cout << "*";
-    //         }
-    //         cout << "\n";
-    //     }
+// {
+//     int n, i, j;
+//     cin >> n;
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = i; j < n; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (j = 1; j <= (2 * i - 1); j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << "\n";
+//     }
+//     for (i = n - 1; i >= 1; i--)
+//     {
+//         for (j = n; j > i; j--)
+//         {
+//             cout << " ";
+//         }
+//         for (j = 1; j <= (2 * i - 1); j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << "\n";
+//     }
 
-    //     return 0;
-    // }
-    // {
-    //     int n, i, j;
-    //     cout << "Enter the number:" << endl;
-    //     cin >> n;
-    //     for (int i = n; i >= 0; i--)
-    //     {
-    //         for (int j = 1; j <= i; j++)
-    //         {
-    //             cout << "* ";
-    //         }
-    //         cout << "\n";
-    //     }
-    //     for (i = 1; i < n; i++)
-    //     {
-    //         for (j = 1; j <= i; j++)
-    //         {
-    //             cout << "* ";
-    //         }
-    //         cout << "\n";
-    //     }
-    // }
-    //     {
-    //         for (s = sp; s > 0; s--)
-    //         {
-    //             /* code */
-    //             cout << " ";
-    //             sp--;
-    //         }
-    //         for (int j = 0; j > 0; j--)
+//     return 0;
+// }
+// {
+//     int n, i, j;
+//     cout << "Enter the number:" << endl;
+//     cin >> n;
+//     for (int i = n; i >= 0; i--)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << "\n";
+//     }
+//     for (i = 1; i < n; i++)
+//     {
+//         for (j = 1; j <= i; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << "\n";
+//     }
+// }
+//     {
+//         for (s = sp; s > 0; s--)
+//         {
+//             /* code */
+//             cout << " ";
+//             sp--;
+//         }
+//         for (int j = 0; j > 0; j--)
 
-    // {
-    //     int n;
-    //     cout << "Enter the size of diamond (number of rows for upper half): ";
-    //     cin >> n;
-    //     for (int i = 1; i <= n; i++)
-    //     {
-    //         for (int j = 1; j <= n - i; j++)
-    //         {
-    //             cout << " ";
-    //         }
-    //         for (int k = 1; k <= 2 * i - 1; k++)
-    //         {
-    //             if (k % 2 == 1)
-    //             {
-    //                 cout << "*";
-    //             }
-    //             else
-    //             {
-    //                 cout << "#";
-    //             }
-    //         }
-    //         cout << endl;
-    //     }
+// {
+//     int n;
+//     cout << "Enter the size of diamond (number of rows for upper half): ";
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int k = 1; k <= 2 * i - 1; k++)
+//         {
+//             if (k % 2 == 1)
+//             {
+//                 cout << "*";
+//             }
+//             else
+//             {
+//                 cout << "#";
+//             }
+//         }
+//         cout << endl;
+//     }
 
-    //     // Lower half of the diamond
-    //     for (int i = n - 1; i >= 1; i--)
-    //     {
-    //         // Print leading spaces
-    //         for (int j = 1; j <= n - i; j++)
-    //         {
-    //             cout << " ";
-    //         }
+//     // Lower half of the diamond
+//     for (int i = n - 1; i >= 1; i--)
+//     {
+//         // Print leading spaces
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << " ";
+//         }
 
-    //                 // Print pattern for each row
-    //         for (int k = 1; k <= 2 * i - 1; k++)
-    //         {
-    //             if (k % 2 == 1)
-    //             {
-    //                 cout << "*";
-    //             }
-    //             else
-    //             {
-    //                 cout << "#";
-    //             }
-    //         }
-    //         cout << endl;
-    //     }
+//                 // Print pattern for each row
+//         for (int k = 1; k <= 2 * i - 1; k++)
+//         {
+//             if (k % 2 == 1)
+//             {
+//                 cout << "*";
+//             }
+//             else
+//             {
+//                 cout << "#";
+//             }
+//         }
+//         cout << endl;
+//     }
 
-    //     return 0;
-    // }
+//     return 0;
+// }
+{
+    int num = 10;
+    if (num > 10)
+    {
+        /* code */
+    }
+}
