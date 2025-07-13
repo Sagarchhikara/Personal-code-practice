40a4248a8a8990e519d8473d419722fcad3f467a
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-13 21:53:46 +0530

    Updates

--- cpp-practice/practise1.cpp
+++ cpp-practice/practise1.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 int m = 10;
 int main(int argc, char const *argv[])
+// {
+//     int m = 20; // redclared locla to main
+//     {
+//         int k = m;
+//         int m = 30;
+//         cout << "We are inner block\n";
+//         cout << "k= " << k << endl;
+//         cout << "M=" << m << endl;
+//         cout << "::m= " << ::m << endl;
+//     }
+//     cout << "/n We are in the outer block\n ";
+//     cout << "M=" << m << endl;
+//     cout << "::m= " << ::m << endl;
+//     return 0;
+// }
 {
-    int m = 20; // redclared locla to main
-    {
-        int k = m;
-        int m = 30;
-        cout << "We are inner block\n";
-        cout << "k= " << k << endl;
-        cout << "M=" << m << endl;
-        cout << "::m= " << ::m << endl;
-    }
-    cout << "/n We are in the outer block\n ";
-    cout << "M=" << m << endl;
-    cout << "::m= " << ::m << endl;
-    return 0;
+    int *arr;
+    int size;
+    cout << "Enter the size of the integer array: ";
+    cin >> size;
+    cout << "Creating an array size of size" << size << "..";
+    arr = new int[size];
+    cout << "\n Dynamic allocation of memory for array is sucessful.";
+    delete arr;
+    getch();
 }
