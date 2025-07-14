7fa33f8ec23eb969ca3861bc6a8ca2b4237c1d88
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-14 14:21:25 +0530

    Updates

--- fee-practice/script.js
+++ fee-practice/script.js
@@ -1,10 +1,4 @@
-let num1 = 50
-let num2 = 50.05
-
-let str1 = "Geek"
-let str2 = 'Geeks'
-
-console.log(num1)
-console.log(num2)
-console.log(str1)
-console.log(str2)
+var a = 10
+{
+    console.log(a)
+}
