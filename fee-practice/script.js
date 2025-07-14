4df5d37604e57148f4b9e8ec5aecf26d73d59012
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-14 15:34:03 +0530

    Update script.js

--- fee-practice/script.js
+++ fee-practice/script.js
@@ -4,4 +4,42 @@ var a = 10
     console.log(a)
 
 }
-console.log(b)
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter a number (1 to 7): ", function (dayNumber) {
+    dayNumber = parseInt(dayNumber);
+
+    switch (dayNumber) {
+        case 1:
+            console.log("Monday");
+            break;
+        case 2:
+            console.log("Tuesday");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5:
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+        case 7:
+            console.log("Sunday");
+            break;
+        default:
+            console.log("Invalid day number. Please enter a number from 1 to 7.");
+    }
+
+    rl.close();
+});
