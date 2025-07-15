93ff9c56c9db175df72daee822e34b79c1c55a88
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-15 14:21:51 +0530

    Update script.js

--- fee-practice/script.js
+++ fee-practice/script.js
@@ -5,41 +5,49 @@
 
 // }
 
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-rl.question("Enter a number (1 to 7): ", function (dayNumber) {
-    dayNumber = parseInt(dayNumber);
+// rl.question("Enter a number (1 to 7): ", function (dayNumber) {
+//     dayNumber = parseInt(dayNumber);
 
-    switch (dayNumber) {
-        case 1:
-            console.log("Monday");
-            break;
-        case 2:
-            console.log("Tuesday");
-            break;
-        case 3:
-            console.log("Wednesday");
-            break;
-        case 4:
-            console.log("Thursday");
-            break;
-        case 5:
-            console.log("Friday");
-            break;
-        case 6:
-            console.log("Saturday");
-            break;
-        case 7:
-            console.log("Sunday");
-            break;
-        default:
-            console.log("Invalid day number. Please enter a number from 1 to 7.");
+//     switch (dayNumber) {
+//         case 1:
+//             console.log("Monday");
+//             break;
+//         case 2:
+//             console.log("Tuesday");
+//             break;
+//         case 3:
+//             console.log("Wednesday");
+//             break;
+//         case 4:
+//             console.log("Thursday");
+//             break;
+//         case 5:
+//             console.log("Friday");
+//             break;
+//         case 6:
+//             console.log("Saturday");
+//             break;
+//         case 7:
+//             console.log("Sunday");
+//             break;
+//         default:
+//             console.log("Invalid day number. Please enter a number from 1 to 7.");
+//     }
+
+//     rl.close();
+// });
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let max = arr[0];
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
     }
-
-    rl.close();
-});
+}
+console.log("Maximum value in the array is:", max);
