a38bd3c9bfd4fff431a828ae8056a3dbb2e52d0c
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-18 10:16:51 +0530

    Update inheritance.cpp

--- cpp-practice/inheritance.cpp
+++ cpp-practice/inheritance.cpp
@@ -22,27 +22,27 @@ using namespace std;
 //     d.sound();
 //     d.bark();
 //     return 0;
-class person
-{
-public:
-    void display(/* args */)
-    {
-        cout << "I am a person\n";
-    }
-};
+// class person
+// {
+// public:
+//     void display(/* args */)
+//     {
+//         cout << "I am a person\n";
+//     }
+// };
 
-class student : public person
-{
-public:
-    void show()
-    {
-        cout << "I am a student.";
-    }
-};
-int main(int argc, char const *argv[])
-{
-    student a;
-    a.display();
-    a.show();
-    return 0;
-}
+// class student : public person
+// {
+// public:
+//     void show()
+//     {
+//         cout << "I am a student.";
+//     }
+// };
+// int main(int argc, char const *argv[])
+// {
+//     student a;
+//     a.display();
+//     a.show();
+//     return 0;
+// }
