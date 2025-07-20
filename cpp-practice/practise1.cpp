c6bbb1957da9338c1550da2d657a3c74474c3ed3
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-20 13:22:05 +0530

    Update practise1.cpp

--- cpp-practice/practise1.cpp
+++ cpp-practice/practise1.cpp
@@ -124,27 +124,67 @@ using namespace std;
 //     }
 // }
 
-void sumofarray(int arr[], int size)
-{
-    int sum = 0;
-    for (int i = 0; i < size; i++)
-    {
-        sum += arr[i];
-    }
-    cout << "The sum is" << sum << endl;
-}
+// void sumofarray(int arr[], int size)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < size; i++)
+//     {
+//         sum += arr[i];
+//     }
+//     cout << "The sum is" << sum << endl;
+// }
 
+// int main(int argc, char const *argv[])
+// {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(arr) / sizeof(int);
+//     // reversearray(arr, size);
+//     // cout << "Reversed array: ";
+//     // for (int i = 0; i < size; i++)
+//     // {
+//     //     cout << arr[i] << " ";
+//     // }
+//     // cout << endl;
+//     sumofarray(arr, size);
+//     return 0;
+// }
+// SWAP MAX AND MIN OF TWO ARRAYS
+void swapmaxmin(int arr[], int size)
+{
+    int maxIndex = 0, minIndex = 0;
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > arr[maxIndex])
+        {
+            maxIndex = i;
+        }
+        if (arr[i] < arr[minIndex])
+        {
+            minIndex = i;
+        }
+    }
+    swap(arr[maxIndex], arr[minIndex]);
+}
 int main(int argc, char const *argv[])
 {
-    int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr) / sizeof(int);
-    // reversearray(arr, size);
-    // cout << "Reversed array: ";
-    // for (int i = 0; i < size; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << endl;
-    sumofarray(arr, size);
+    int arr[] = {3, 1, 4, 1, 5, 9, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    swapmaxmin(arr, size);
+
+    cout << "Array after swapping max and min: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
