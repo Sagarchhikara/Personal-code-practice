663a1b1048afd1e8b5a9b1084c39804840a68cea
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-20 12:41:21 +0530

    Update practise1.cpp

--- cpp-practice/practise1.cpp
+++ cpp-practice/practise1.cpp
@@ -72,25 +72,25 @@ using namespace std;
 
 //     return 0;
 // }
-void changearr(int arr[], int size)
-{
-    cout << "In function\n";
-    for (int i = 0; i < size; i++)
-    {
-        /* code */
-        arr[i] = 2 * arr[i];
-    }
-}
-int main(int argc, char const *argv[])
-{
-    int arr[] = {1, 2, 3};
-    changearr(arr, 3);
-    cout << "In main\n";
-    for (int i = 0; i < 3; i++)
-    {
-        /* code */
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    return 0;
-}
+// void changearr(int arr[], int size)
+// {
+//     cout << "In function\n";
+//     for (int i = 0; i < size; i++)
+//     {
+//         /* code */
+//         arr[i] = 2 * arr[i];
+//     }
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int arr[] = {1, 2, 3};
+//     changearr(arr, 3);
+//     cout << "In main\n";
+//     for (int i = 0; i < 3; i++)
+//     {
+//         /* code */
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
