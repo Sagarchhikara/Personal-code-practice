46eb673a987d4006d16f9918c47f1b3f0c1f92a1
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-20 13:13:37 +0530

    Update practise1.cpp

--- cpp-practice/practise1.cpp
+++ cpp-practice/practise1.cpp
@@ -94,22 +94,45 @@ using namespace std;
 //     cout << endl;
 //     return 0;
 // }
-int linearsearch(int arr[], int size, int target)
+// int linearsearch(int arr[], int size, int target)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         /* code */
+//         if (arr[i] == target)
+//             return i;
+//     }
+//     return -1;
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int arr[] = {4, 7, 3, 1, 2, 9};
+//     int size = 6;
+//     int target = 2;
+//     cout << "Searching for " << target << " in the array...\n";
+//     cout << linearsearch(arr, size, target);
+//     return 0;
+// }
+void reversearray(int arr[], int size)
 {
-    for (int i = 0; i < size; i++)
+    int start = 0, end = size - 1;
+    while (start < end)
     {
-        /* code */
-        if (arr[i] == target)
-            return i;
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
     }
-    return -1;
 }
 int main(int argc, char const *argv[])
 {
-    int arr[] = {4, 7, 3, 1, 2, 9};
-    int size = 6;
-    int target = 2;
-    cout << "Searching for " << target << " in the array...\n";
-    cout << linearsearch(arr, size, target);
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(int);
+    reversearray(arr, size);
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
