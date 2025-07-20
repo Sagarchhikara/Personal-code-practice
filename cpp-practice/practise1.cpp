a80cb585abb78c4956baa02577e25d02188b566f
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-20 12:29:27 +0530

    Updates

--- cpp-practice/practise1.cpp
+++ cpp-practice/practise1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using namespace std;
+
 // #include <conio.h>
 // #include <iomanip>
 // using namespace std;
@@ -37,19 +39,36 @@
 //     cout << setw(10) << "Total" << setw(10) << Total << endl;
 //     return 0;
 // }
-using namespace std;
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int marks[5] = {99, 100, 200, 300};
+//     int price[] = {200, 300};
+//     int size = sizeof(marks) / sizeof(int); // memory of array
+//     cout << marks[0] << endl;
+//     cout << size << endl;
+//     for (int i = 0; i < size; i++)
+//     {
+//         /* code */
+//         cout << marks[i] << endl;
+//     }
+
+//     return 0;
+// }
 int main(int argc, char const *argv[])
 {
-    int marks[5] = {99, 100, 200, 300};
-    int price[] = {200, 300};
-    int size = sizeof(marks) / sizeof(int); // memory of array
-    cout << marks[0] << endl;
-    cout << size << endl;
+    int nums[] = {3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(nums) / sizeof(int);
+    int smallest = INT16_MAX;
     for (int i = 0; i < size; i++)
     {
         /* code */
-        cout << marks[i] << endl;
+        if (nums[i] < INT16_MAX)
+        {
+            smallest = nums[i];
+        }
     }
+    cout << "Smallest= " << smallest << endl;
 
     return 0;
 }
