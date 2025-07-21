9e60da871896e9ccc7d5219a9b0c213b852112d3
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-21 11:35:22 +0530

    Update script.js

--- fee-practice/script.js
+++ fee-practice/script.js
@@ -77,11 +77,11 @@
 // console.log(o)
 // o.salary = "500 crore"
 // console.log(o)
-console.log("Hello I am conditional tutorial")
-let age = 45;
-if (age > 18) {
-    console.log("You can drive")
-}
-else {
-    console.log("You cannot drive")
-}
+// console.log("Hello I am conditional tutorial")
+// let age = 45;
+// if (age > 18) {
+//     console.log("You can drive")
+// }
+// else {
+//     console.log("You cannot drive")
+// }
