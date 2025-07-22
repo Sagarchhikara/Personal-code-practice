54175475c000172081ddd85ccd2312f3d82b19d6
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-22 09:49:51 +0530

    Update inheritance.cpp

--- cpp-practice/inheritance.cpp
+++ cpp-practice/inheritance.cpp
@@ -153,25 +153,28 @@ using namespace std;
 //     obj.accessA(); // Accessing A's method through B
 //     return 0;
 // }
-class vehicle
-{
-public:
-    vehicle()
-    {
-        cout << "This is a vehicle";
-    }
-};
-class car : public vehicle
-{
-public:
-    car()
-    {
-        cout << "This is a car";
-    }
-};
-int main(int argc, char const *argv[])
-{
-    car obj;
+// class vehicle
+// {
+// public:
+//     vehicle()
+//     {
+//         cout << "This is a vehicle";
+//     }
+// };
+// class car : public vehicle
+// {
+// public:
+//     car()
+//     {
+//         cout << "This is a car";
+//     }
+// };
+// int main(int argc, char const *argv[])
+// {
+//     car obj;
 
-    return 0;
+//     return 0;
+// }
+class vehicle{
+    
 }
