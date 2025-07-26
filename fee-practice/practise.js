617d3329b0d58da59ff883e6938f2870f4f0284f
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-26 17:25:53 +0530

    Update practise.js

--- fee-practice/practise.js
+++ fee-practice/practise.js
@@ -1,25 +1,29 @@
-const faultyCalculator = (a, b) => {
-    console.log("a is " + a);
-    console.log("b is " + b);
+// const faultyCalculator = (a, b) => {
+//     console.log("a is " + a);
+//     console.log("b is " + b);
 
-    let random = Math.random();
-    let Addition = a + b;
-    let Subtraction = a - b;
-    let Multiplication = a * b;
-    let Division = a / b;
-    let Exponentiation = a ** b;
+//     let random = Math.random();
+//     let Addition = a + b;
+//     let Subtraction = a - b;
+//     let Multiplication = a * b;
+//     let Division = a / b;
+//     let Exponentiation = a ** b;
 
-    if (random < 0.1) {
-        Addition = a - b;
-        Subtraction = a / b;
-        Multiplication = a + b;
-        Division = a ** b;
-    }
+//     if (random < 0.1) {
+//         Addition = a - b;
+//         Subtraction = a / b;
+//         Multiplication = a + b;
+//         Division = a ** b;
+//     }
 
-    console.log("Addition Result after faulty calculation: " + Addition);
-    console.log("SubtractionResult after faulty calculation: " + Subtraction);
-    console.log("MultiplicationResult after faulty calculation: " + Multiplication);
-    console.log("DivisionResult after faulty calculation: " + Division);
-    console.log("ExponentiationResult after faulty calculation: " + Exponentiation);
-};
-faultyCalculator(1, 1);
+//     console.log("Addition Result after faulty calculation: " + Addition);
+//     console.log("SubtractionResult after faulty calculation: " + Subtraction);
+//     console.log("MultiplicationResult after faulty calculation: " + Multiplication);
+//     console.log("DivisionResult after faulty calculation: " + Division);
+//     console.log("ExponentiationResult after faulty calculation: " + Exponentiation);
+// };
+// faultyCalculator(1, 1);
+console.log("This is string tutorila")
+let a = "SagarChhikara"
+console.log(a[0])
+console.log(a[1])
