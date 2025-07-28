cef38bd74aa1a53b9b389d1e9b81a9690a9a167b
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-28 10:17:50 +0530

    Update lec5.cpp

--- cpp-practice/lec5.cpp
+++ cpp-practice/lec5.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 using namespace std;
 
+// class demo
+// {
+// public:
+//     demo()
+//     {
+//         cout << "Constructor called";
+//     }
+// };
+// int main(int argc, char const *argv[])
+// {
+//     demo d;
+//     cout << endl;
+//     cout << "End of main function";
+//     return 0;
+// }
+// Destructor demo
 class demo
 {
 public:
-    demo()
+    ~demo()
     {
-        cout << "Constructor called";
+        cout << "Destructor called";
     }
 };
 int main(int argc, char const *argv[])
