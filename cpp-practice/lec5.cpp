50852d284b1f605bc3be4fdeb6b187b1812f6ea0
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-28 10:10:14 +0530

    updates

---
+++ cpp-practice/lec5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class demo
+{
+public:
+    demo()
+    {
+        cout << "Constructor called";
+    }
+};
+int main(int argc, char const *argv[])
+{
+    demo d;
+    cout << endl;
+    cout << "End of main function";
+    return 0;
+}
