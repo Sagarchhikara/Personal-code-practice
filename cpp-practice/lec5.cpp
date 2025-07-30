b7f415983c8612d4ee804068ae797b9a47473a84
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-30 22:36:36 +0530

    Update lec5.cpp

--- cpp-practice/lec5.cpp
+++ cpp-practice/lec5.cpp
@@ -61,16 +61,17 @@ using namespace std;
 // }
 // STATIC OBJECT
 // Function definition
-void create()
-{
-    static int x = 0; // Static variable retains its value across function calls
-    x++;              // Increment the static variable
-    cout << "x: " << x << endl;
-    // Display the value of x
-}
-int main()
-{
-    create(); // First call: x becomes 1, output -> x: 1
-    create(); // Second call: x becomes 2, output -> x: 2
-    return 0; // End of program
-}
+// void create()
+// {
+//     static int x = 0; // Static variable retains its value across function calls
+//     x++;              // Increment the static variable
+//     cout << "x: " << x << endl;
+//     // Display the value of x
+// }
+// int main()
+// {
+//     create(); // First call: x becomes 1, output -> x: 1
+//     create(); // Second call: x becomes 2, output -> x: 2
+//     return 0; // End of program
+// }
+// CONSTANT MEMBER FUNCTION
