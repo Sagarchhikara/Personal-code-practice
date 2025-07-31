3d9e2c7212f8b70a042d6bebd2ad0a115ee3f525
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-07-31 10:31:00 +0530

    Update lec5.cpp

--- cpp-practice/lec5.cpp
+++ cpp-practice/lec5.cpp
@@ -204,3 +204,45 @@ using namespace std;
 //     }
 //     return 0;
 // }
+class calc
+{
+    int a, b;
+    char op;
+
+public:
+    void run()
+    {
+        cout << "Enter first number:";
+        cin >> a;
+        cout << "Enter second number:";
+        cin >> b;
+        cout << "Enter operator:";
+        cin >> op;
+        switch (op)
+        {
+        case '+':
+            cout << "Result: " << (a + b);
+            break;
+        case '-':
+            cout << "Result: " << (a - b);
+            break;
+        case '*':
+            cout << "Result: " << (a * b);
+            break;
+        case '/':
+            if (b != 0)
+                cout << "Result: " << (a / b);
+            else
+                cout << "Division by zero error";
+            break;
+        default:
+            cout << "Invalid operator";
+        }
+    }
+};
+int main()
+{
+    calc c;
+    c.run();
+    return 0;
+}
