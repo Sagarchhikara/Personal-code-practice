1059b0b1c8c2e5d8de5ce44216c98a0f02d8eed3
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-01 10:10:18 +0530

    Update lec6.cpp

--- cpp-practice/lec6.cpp
+++ cpp-practice/lec6.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 using namespace std;
-class counter
+// class counter
+// {
+// private:
+//     static int count;
+
+// public:
+//     counter()
+//     {
+//         count++;
+//     }
+//     static void displaycount()
+//     {
+//         cout << "The number of objects created: " << count << endl;
+//     }
+// };
+// int counter::count = 0;
+// int main()
+// {
+//     counter c1;
+//     counter c2;
+//     counter c3;
+//     counter::displaycount();
+//     return 0;
+// }
+class book
 {
-private:
-    static int count;
+    string title;
+    string author;
+    int pages;
 
 public:
-    counter()
+    book(string t, string a, int p) : title(t), author(a), pages(p) {}
+    void display() const
     {
-        count++;
-    }
-    static void displaycount()
-    {
-        cout << "The number of objects created: " << count << endl;
+        cout << "Book title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Pages: " << pages << endl;
     }
 };
-int counter::count = 0;
 int main()
 {
-    counter c1;
-    counter c2;
-    counter c3;
-    counter::displaycount();
+    const book mybook("The alchemist", "Paulo coehlo", 208);
+    mybook.display();
     return 0;
 }
