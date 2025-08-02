236cb9ce24b65a78b216c85c670212d3744e0c9c
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-02 16:40:10 +0530

    Update leetcode.cpp

--- cpp-practice/leetcode.cpp
+++ cpp-practice/leetcode.cpp
@@ -12,3 +12,28 @@ public:
         return ans;
     }
 };
+// question 169 moores voting algo
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int frq = 0, ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (frq == 0)
+            {
+                ans = nums[i];
+            }
+            if (ans == nums[i])
+            {
+                frq++;
+            }
+            else
+            {
+                frq--;
+            }
+        }
+        return ans;
+    }
+};
