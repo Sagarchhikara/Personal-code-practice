16137858be597a6c5e55e01f4d26d7e391fba2fe
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-08-03 23:10:57 +0530

    Simplify lec1.cpp and remove duplicate file
    Replaced commented code in cpp-practice/lec1.cpp with a concise arithmetic example. Deleted duplicate lec1.cpp from the root directory to avoid redundancy.

--- cpp-practice/lec1.cpp
+++ cpp-practice/lec1.cpp
@@ -1,91 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-// int main()
-// {
-//     cout << "Hello, World!" << endl;
-//     cout << "This is a C++14 program." << endl;
-//     return 0;
-// }
-// int main()
-// {
-//     int n = 5;
-//     int arr[4] = {5, 1, 3, 2};
-//     int missingNumber = 0;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         missingNumber ^= i;
-//     }
-//     for (int i = 0; i < n - 1; i++)
-//     {
-//         missingNumber ^= arr[i];
-//     }
-//     cout << missingNumber;
-// }
-// int main(){
-//     int n=78;
-//     if (n==0) return -1;
-//     int ans=1;
-//     while (n%2==0){
-//         n=n>>1;
-//         ans++;
-//     }
-//     cout<<ans;
-
-// }
-// input a number in cpp
-// int main()
-// {
-//     int num;
-//     cout << "Enter a number:";
-//     cin >> num;
-//     cout << "Entered number is" << num;
-//     return 0;
-// }
-// int main(int argc, char const *argv[])
-// {
-//     int num1, num2;
-//     cout << "ENter number one:";
-//     cin >> num1;
-//     cout << "Enter number two:";
-//     cin >> num2;
-//     cout << "Sum is:" << num1 + num2;
-//     cout << "\nDifference is:" << num1 - num2;
-//     return 0;
-// }
-// int main()
-// {
-//     int age;
-//     cout << "Enter your age:\n";
-//     cin >> age;
-//     if (age >= 18)
-//     {
-//         cout << "You are eligible to drive\n";
-//     }
-//     else
-//     {
-//         cout << "You have to be atleast 18 to be eligible.";
-//     }
-//     return 0;
-// }
-// Display odd and even number in two columns
-// int main()
-// {
-//     cout << "Odd\tEven\n";
-//     int odd = 1, even = 2;
-//     while (odd <= 25 || even <= 25)
-//     {
-//         if (odd <= 25)
-//             cout << odd;
-//         else
-//             cout << " ";
-//         cout << "\t";
-//         if (even <= 25)
-//             cout << even;
-//         cout << endl;
-//         odd += 2;
-//         even += 2;
-//     }
-//     return 0;
-// }
-//
+int main(){
+   int a=5, b=10;
+   cout<<"Sum="<<a+b<<endl;
+    cout<<"Difference="<<b-a<<endl;
+    cout<<"Product="<<a*b<<endl;
+    cout<<"Quotient="<<b/a<<endl;
+    return 0;
+}
