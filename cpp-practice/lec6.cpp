1c9445faca1a8cd43f7d4ea131dc81b311965583
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-07 10:16:02 +0530

    Comment out main and Solution class implementations
    The main function in lec6.cpp and the Solution class for majority element in practise1.cpp have been commented out, likely for testing or refactoring purposes. No functional code remains active in these sections.

--- cpp-practice/lec6.cpp
+++ cpp-practice/lec6.cpp
@@ -250,37 +250,38 @@ using namespace std;
 
 // Overloaded functions
 //
-int main()
-{
-    // Simple lambda
-    auto greet = []()
-    {
-        cout << "Hello from lambda!" << endl;
-    };
-    greet();
+// int main()
+// {
+//     // Simple lambda
+//     auto greet = []()
+//     {
+//         cout << "Hello from lambda!" << endl;
+//     };
+//     greet();
 
-    // Lambda with parameters
-    auto add = [](int a, int b)
-    {
-        return a + b;
-    };
-    cout << "5 + 3 = " << add(5, 3) << endl;
+//     // Lambda with parameters
+//     auto add = [](int a, int b)
+//     {
+//         return a + b;
+//     };
+//     cout << "5 + 3 = " << add(5, 3) << endl;
 
-    // Lambda with capture
-    int x = 10;
-    auto captureExample = [x](int y)
-    {
-        cout << "Captured x: " << x << ", parameter y: " << y << endl;
-    };
-    captureExample(20);
+//     // Lambda with capture
+//     int x = 10;
+//     auto captureExample = [x](int y)
+//     {
+//         cout << "Captured x: " << x << ", parameter y: " << y << endl;
+//     };
+//     captureExample(20);
 
-    // Using lambda with STL algorithm
-    vector<int> numbers = {1, 2, 3, 4, 5};
-    cout << "Even numbers: ";
-    for_each(numbers.begin(), numbers.end(), [](int n)
-             {
-        if (n % 2 == 0) cout << n << " "; });
-    cout << endl;
+//     // Using lambda with STL algorithm
+//     vector<int> numbers = {1, 2, 3, 4, 5};
+//     cout << "Even numbers: ";
+//     for_each(numbers.begin(), numbers.end(), [](int n)
+//              {
+//         if (n % 2 == 0) cout << n << " "; });
+//     cout << endl;
 
-    return 0;
-}
+//     return 0;
+// }
+int x = 5, y =
