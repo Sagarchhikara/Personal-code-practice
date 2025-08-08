4c0925b30fd442e044a6e0a5e9fa9a0436592e71
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-08 10:27:49 +0530

    Update recursion.cpp

--- cpp-practice/recursion.cpp
+++ cpp-practice/recursion.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 using namespace std;
-void print(int n)
+// void print(int n)
+// {
+//     if (n == 0)
+//         return;
+//     cout << n << " ";
+//     print(n - 1);
+// }
+// int main(int argc, char const *argv[])
+// {
+//     print(5);
+//     return 0;
+// }
+// factorial using recurion
+int factorial(int n)
 {
-    if (n == 0)
-        return;
-    cout << n << " ";
-    print(n - 1);
+    if (n <= 1)
+        return 1;
+    return n * factorial(n - 1);
 }
-int main(int argc, char const *argv[])
+int main()
 {
-    print(5);
+    cout << "Enter the number you want factorial of:\n";
+    int a;
+    cin >> a;
+    int result = factorial(a);
+    cout << result << endl;
     return 0;
 }
