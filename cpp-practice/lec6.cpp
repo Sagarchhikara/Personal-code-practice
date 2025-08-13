40b9d52b475a678917d66d4e3db66447b97b7843
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-08-13 10:43:05 +0530

    Update lec6.cpp

--- cpp-practice/lec6.cpp
+++ cpp-practice/lec6.cpp
@@ -315,3 +315,10 @@ using namespace std;
 //     cout<<"Right child: "<<root->right->data<<endl;
 //     return 0;
 // }
+int main(){
+    int arr[5]={1, 2, 3, 4, 5};
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
