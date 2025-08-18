9aa99a069b26ecdb5a9810746a13263c8d14efc4
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-18 10:08:31 +0530

    Update lec7.cpp

--- cpp-practice/lec7.cpp
+++ cpp-practice/lec7.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+int sum(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    int prevsum = sum(n - 1);
+    return n + prevsum;
+}
+int main()
+{
+    int a = 20;
+    int result = sum(a);
+    cout << result << endl;
+    return 0;
+}
