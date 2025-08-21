7b37be20e80428f54458b86185f1c89a944151e5
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-21 10:08:44 +0530

    Update lec7.cpp

--- cpp-practice/lec7.cpp
+++ cpp-practice/lec7.cpp
@@ -36,48 +36,48 @@ using namespace std;
 //     cout << result << endl;
 //     return 0;
 // }
-// #include <iostream>
-// #include <string>
-// using namespace std;
+#include <iostream>
+#include <string>
+using namespace std;
 
-// int growthvalue(string func)
-// {
-//     if (func == "i")
-//         return 0; // constant
-//     if (func == "log(n)")
-//         return 1; // logarithmic
-//     if (func == "n")
-//         return 2; // linear
-//     if (func == "n logn")
-//         return 3; // linearithmic
-//     if (func == "n^2")
-//         return 4; // quadratic
-//     if (func == "n^3")
-//         return 5; // cubic
-//     return -1;    // unknown
-// }
+int growthvalue(string func)
+{
+    if (func == "i")
+        return 0; // constant
+    if (func == "log(n)")
+        return 1; // logarithmic
+    if (func == "n")
+        return 2; // linear
+    if (func == "n logn")
+        return 3; // linearithmic
+    if (func == "n^2")
+        return 4; // quadratic
+    if (func == "n^3")
+        return 5; // cubic
+    return -1;    // unknown
+}
 
-// int main(int argc, char const *argv[])
-// {
-//     string notation, f_n, g_n;
-//     cin >> notation >> f_n >> g_n;
+int main(int argc, char const *argv[])
+{
+    string notation, f_n, g_n;
+    cin >> notation >> f_n >> g_n;
 
-//     int f = growthvalue(f_n);
-//     int g = growthvalue(g_n);
+    int f = growthvalue(f_n);
+    int g = growthvalue(g_n);
 
-//     bool result = false;
+    bool result = false;
 
-//     if (notation == "O") // Big-O
-//         result = (f <= g);
-//     else if (notation == "o") // little-o
-//         result = (f < g);
-//     else if (notation == "Ω") // Big-Omega
-//         result = (f >= g);
-//     else if (notation == "ω") // little-omega
-//         result = (f > g);
-//     else if (notation == "Θ") // Theta
-//         result = (f == g);
+    if (notation == "O") // Big-O
+        result = (f <= g);
+    else if (notation == "o") // little-o
+        result = (f < g);
+    else if (notation == "Ω") // Big-Omega
+        result = (f >= g);
+    else if (notation == "ω") // little-omega
+        result = (f > g);
+    else if (notation == "Θ") // Theta
+        result = (f == g);
 
-//     cout << (result ? "True" : "False") << endl;
-//     return 0;
-// }
+    cout << (result ? "True" : "False") << endl;
+    return 0;
+}
