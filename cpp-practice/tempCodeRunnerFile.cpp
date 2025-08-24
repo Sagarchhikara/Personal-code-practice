95da3be566c81b1e444d038f6ce81b3802c10d77
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-24 20:51:13 +0530

    auto: update 2025-08-24 20:51:13

---
+++ cpp-practice/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+void tailrec(int n)
+// {
+//     if (n == 0)
+//         return;
+//     cout << n << " ";
+//     tailrec(n - 1);
+// }
+// int main()
+// {
+//     tailrec(5);
+//     return 0;
+// }
