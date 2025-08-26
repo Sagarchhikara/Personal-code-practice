559a523d1d69ce5bde351fd4946fd961e6454b09
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-26 10:00:10 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/lec1.java
+++ java-practice/lec1.java
@@ -1,7 +1,7 @@
 public class lec1 {
     public static void main(String[] args) {
         int num = 10;
-        system.out.println("Value of a list:" + num);
-        system.out.println("Hello continuing program....");
+        System.out.println("Value of a list:" + num);
+        System.out.println("Hello continuing program....");
     }
 }
