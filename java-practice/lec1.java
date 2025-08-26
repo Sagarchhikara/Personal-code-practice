575eaa65764b1a4bd1013bba6e7d3f88d62b1aa3
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-26 10:34:46 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/lec1.java
+++ java-practice/lec1.java
@@ -1,7 +1,8 @@
-public class lec1 {
-    public static void main(String[] args) {
-        int num = 10;
-        System.out.println("Value of a list:" + num);
-        System.out.println("Hello continuing program....");
+class stud {
+    public static void main(string args[])
+    {
+        int num;
+        System.out.println("Enter the roll number: ");
+        Scanner
     }
 }
