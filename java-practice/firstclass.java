8982d6bd9e2851038287f1c68897925bd592ebdb
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-26 22:04:42 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/firstclass.java
+++ java-practice/firstclass.java
@@ -1,7 +1,7 @@
 public class firstclass {
     public static void main(String[] args) {
         // output in java
-        System.out.println("Hello World with java ");
+        System.out.println("*\n**\n***\n****\n ");
         System.out.println("Hello World with java ");
 
     }
