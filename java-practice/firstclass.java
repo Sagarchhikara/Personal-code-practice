48969214c22392701fd58b02e023aed08d071f14
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-29 10:14:49 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/firstclass.java
+++ java-practice/firstclass.java
@@ -1,18 +1,18 @@
 import  java.util.Scanner;
-class stud{
-    public  static  void main(String args[]){
-        int num;
-        System.out.println("enter the roll  number");
-        Scanner sc=new Scanner(System.in);
-        num=sc.nextInt();
-        String name;
-        System.out.println("enter the name  ");
-        name=sc.next();
-        System.out.println("Hello "+name);
-        float marks;
-        System.out.println("enter the marks ");
-        marks=sc.nextFloat();
-        System.out.println("Entered marks "+marks);
+// class stud{
+//     public  static  void main(String args[]){
+//         int num;
+//         System.out.println("enter the roll  number");
+//         Scanner sc=new Scanner(System.in);
+//         num=sc.nextInt();
+//         String name;
+//         System.out.println("enter the name  ");
+//         name=sc.next();
+//         System.out.println("Hello "+name);
+//         float marks;
+//         System.out.println("enter the marks ");
+//         marks=sc.nextFloat();
+//         System.out.println("Entered marks "+marks);
 
-    }
+//     }
 }
