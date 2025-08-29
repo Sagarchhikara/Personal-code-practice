52be34f3d5ff44fc3abe16462ec9ce0d0e547539
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-29 14:33:02 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/TestEmployee.java
+++ java-practice/TestEmployee.java
@@ -1,3 +1,13 @@
-public class TestEmployee {
+package edu.chitkara.training.java.core.corejava.basics;
 
+public class TestEmployee {
+    public static void main(String[] args) {
+        Employee e1 = new Employee(10, 20);
+        e1.show();
+        Employee e2 = new Employee(30, 40);
+        e2.show();
+        Employee e3 = new Employee(50, 60);
+        e3.show();
+        Employee.showstatic();
+    }
 }
