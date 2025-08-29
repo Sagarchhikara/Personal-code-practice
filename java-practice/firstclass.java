fadfcbb635913026861be20b73e6deac410270f4
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-29 10:47:17 +0530

    Update firstclass.java

--- java-practice/firstclass.java
+++ java-practice/firstclass.java
@@ -1,26 +1,24 @@
 import  java.util.Scanner;
- class stud{
-     public  static  void main(String args[]){
-         int num;
-         System.out.println("enter the roll  number");
-         Scanner sc=new Scanner(System.in);
-         num=sc.nextInt();
-         String name;
-         System.out.println("enter the name  ");
-         name=sc.next();
-         System.out.println("Hello "+name);
-         float marks;
-         System.out.println("enter the marks ");
-         marks=sc.nextFloat();
-         System.out.println("Entered marks "+marks);
-
-     } }
+// class stud{
+//     public  static  void main(String args[]){
+//         int num;
+//         System.out.println("enter the roll  number");
+//         Scanner sc=new Scanner(System.in);
+//         num=sc.nextInt();
+//         String name;
+//         System.out.println("enter the name  ");
+//         name=sc.next();
+//         System.out.println("Hello "+name);
+//         float marks;
+//         System.out.println("enter the marks ");
+//         marks=sc.nextFloat();
+//         System.out.println("Entered marks "+marks);
+//
+//     } }
 // class main{
 //     int a,b;
 //     Scanner sc=new Scanner(System.in);
 //     System.out.println("Enter first number:")
 //     a=sc.nextInt();
 //     b=sc.nextInt();
-
-
-}
+// wap to accept two integers and demostratte relational operators
