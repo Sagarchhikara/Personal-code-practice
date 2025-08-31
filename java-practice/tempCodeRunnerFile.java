9034d0d44652bc59ce1bf72a76e744212f0d6160
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-08-31 09:31:28 +0530

    Create tempCodeRunnerFile.java

---
+++ java-practice/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+  int a, b;
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter 1st number:");
+    //     a = sc.nextInt();
+    //     System.out.println("Eter second number:");
+    //     b = sc.nextInt();
+    //     System.out.println("Sum: " + (a + b));
+    // }
