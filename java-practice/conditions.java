d78849d93c152bb6aa48f2de67b276e3e01aabe3
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-02 13:11:44 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/conditions.java
+++ java-practice/conditions.java
@@ -1,14 +1,35 @@
-import  java.util.*;
-public class conditions{
-    public  static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter age: ");
-        int age =sc.nextInt();
-        if(age>18){
-            System.out.println("Adult");
-        }
-        else {
-            System.out.println("Child");
-        }
+import java.util.*;
+
+public class conditions {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter age: ");
+        // int age =sc.nextInt();
+        // if(age>18){
+        // System.out.println("Adult");
+        // // }
+        // // else {
+        // // System.out.println("Child");
+        // // }
+        // System.out.println("Enter 1st number:");
+        // int a=sc.nextInt();
+        // System.out.println("Enter 2nd number:");
+        // int b=sc.nextInt();
+        // if(a==b){
+        // System.out.println("Equal");
+        // }
+        // else if (a>b){
+        // System.out.println("Greater");
+        // }
+        // else {
+        // System.out.println("Less");
+        // }
+        // // if(n%2==0){
+        // // System.out.println("The number is even");
+        // // }
+        // // else{
+        // // System.out.println("The number is odd");
+        // // }
+
     }
 }
