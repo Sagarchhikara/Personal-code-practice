562ca26524301e8a1600f43c349572f9185fed39
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-02 12:58:39 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-  int a, b;
-    //     Scanner sc = new Scanner(System.in);
-    //     System.out.println("Enter 1st number:");
-    //     a = sc.nextInt();
-    //     System.out.println("Eter second number:");
-    //     b = sc.nextInt();
-    //     System.out.println("Sum: " + (a + b));
-    // }
