cbf85618df27cab2d43e279fe47829318cc39936
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-03 14:11:35 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/conditions.java
+++ java-practice/conditions.java
@@ -1,57 +1,93 @@
 import java.util.*;
 
+// public class conditions {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         // System.out.print("Enter age: ");
+//         // int age =sc.nextInt();
+//         // if(age>18){
+//         // System.out.println("Adult");
+//         // // }
+//         // // else {
+//         // // System.out.println("Child");
+//         // // }
+//         // System.out.println("Enter 1st number:");
+//         // int a=sc.nextInt();
+//         // System.out.println("Enter 2nd number:");
+//         // int b=sc.nextInt();
+//         // if(a==b){
+//         // System.out.println("Equal");
+//         // }
+//         // else if (a>b){
+//         // System.out.println("Greater");
+//         // }
+//         // else {
+//         // System.out.println("Less");
+//         // }
+//         // // if(n%2==0){
+//         // // System.out.println("The number is even");
+//         // // }
+//         // // else{
+//         // // System.out.println("The number is odd");
+//         // // }
+//         // System.out.println("Enter a number 1 ,2 ,3: ");
+//         // int num = sc.nextInt();
+//         // if (num == 1) {
+//         // System.out.println("Hello!");
+//         // } else if (num == 2) {
+//         // System.out.println("Namaste");
+//         // } else if (num == 3) {
+//         // System.out.println("Bonjour");
+//         // } else {
+//         // System.out.println("Invalid input");
+//         // }
+//         // switch (num) {
+//         // case 1:
+//         // System.out.println("hello");
+//         // break;
+//         // case 2:
+//         // System.out.println("namste");
+//         // break;
+//         // case 3:
+//         // System.out.println("bonjour");
+//         // default:
+//         // System.out.println("Invaluid input ");
+//         // break;
+//     }
+// }
+
 public class conditions {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter age: ");
-        // int age =sc.nextInt();
-        // if(age>18){
-        // System.out.println("Adult");
-        // // }
-        // // else {
-        // // System.out.println("Child");
-        // // }
-        // System.out.println("Enter 1st number:");
-        // int a=sc.nextInt();
-        // System.out.println("Enter 2nd number:");
-        // int b=sc.nextInt();
-        // if(a==b){
-        // System.out.println("Equal");
-        // }
-        // else if (a>b){
-        // System.out.println("Greater");
-        // }
-        // else {
-        // System.out.println("Less");
-        // }
-        // // if(n%2==0){
-        // // System.out.println("The number is even");
-        // // }
-        // // else{
-        // // System.out.println("The number is odd");
-        // // }
-        // System.out.println("Enter a number 1 ,2 ,3: ");
-        // int num = sc.nextInt();
-        // if (num == 1) {
-        // System.out.println("Hello!");
-        // } else if (num == 2) {
-        // System.out.println("Namaste");
-        // } else if (num == 3) {
-        // System.out.println("Bonjour");
-        // } else {
-        // System.out.println("Invalid input");
-        // }
-        // switch (num) {
-        // case 1:
-        // System.out.println("hello");
-        // break;
-        // case 2:
-        // System.out.println("namste");
-        // break;
-        // case 3:
-        // System.out.println("bonjour");
-        // default:
-        // System.out.println("Invaluid input ");
-        // break;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b=sc.nextInt();
+        System.out.println("Enter the number of operation:");
+        int num =sc.nextInt();
+        switch (num) {
+            case 1:
+            System.out.println("The sum of nmber is"+(a+b));
+            break;
+            case 2:
+            System.out.println("The subtraction of number is:"+(a-b));
+            break;
+            case 3:
+            System.out.println("The multiplication of number is"+(a*b));
+            
+            break;ase 1:
+            
+            break;ase 1:
+            
+            break;ase 1:
+            
+            break;ase 1:
+            
+            break;
+        
+            default:
+                break;
+        }
+
     }
 }
