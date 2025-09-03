fe63cb092d0aaadc855c59a4d72a3d56865446e5
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-03 13:57:53 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/conditions.java
+++ java-practice/conditions.java
@@ -30,6 +30,16 @@ public class conditions {
         // // else{
         // // System.out.println("The number is odd");
         // // }
-
+        System.out.println("Enter a number 1 ,2 ,3: ");
+        int num = sc.nextInt();
+        if (num == 1) {
+            System.out.println("Hello!");
+        } else if (num == 2) {
+            System.out.println("Namaste");
+        } else if (num == 3) {
+            System.out.println("Bonjour");
+        } else {
+            System.out.println("Invalid input");
+        }
     }
 }
