5130ac1e4bc07acc2bbb1914832455ce183c2c51
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-09 21:24:22 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/sum.java
+++ java-practice/sum.java
@@ -3,27 +3,33 @@ import java.util.*;;
 public class Sum {
     public static void main(String[] args) {
 
-        int ans = sum2();
-        System.out.println(ans);
+        // int ans = sum2();
+        // System.out.println(ans);
+        String message = greet();
+        System.out.println(message);
 
     }
 
-    // return value
-    static int sum2() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter the second number:");
-        int b = sc.nextInt();
-        return (a + b);
-    }
+    // // return value
+    // static int sum2() {
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter the first number:");
+    // int a = sc.nextInt();
+    // System.out.println("Enter the second number:");
+    // int b = sc.nextInt();
+    // return (a + b);
+    // }
 
-    static void sum() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter the second number:");
-        int b = sc.nextInt();
-        System.out.println("The sum of numbers is " + (a + b));
+    // static void sum() {
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter the first number:");
+    // int a = sc.nextInt();
+    // System.out.println("Enter the second number:");
+    // int b = sc.nextInt();
+    // System.out.println("The sum of numbers is " + (a + b));
+    // }
+    static String greet() {
+        String greeting = "how are you ";
+        return greeting;
     }
 }
