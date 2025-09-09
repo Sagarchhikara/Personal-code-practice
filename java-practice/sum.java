f4e1864ad9f2147cdcb77cd3c6f8a0e78684ceb3
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-09 21:11:56 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/sum.java
+++ java-practice/sum.java
@@ -3,10 +3,21 @@ import java.util.*;;
 public class Sum {
     public static void main(String[] args) {
         sum();
+        int ans = sum2();
+        System.out.println(ans);
 
     }
-    static void sum2(){
-        
+
+    // return value
+    static int sum2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b = sc.nextInt();
+        return (a + b);
+    }
+
     }
 
     static void sum() {
