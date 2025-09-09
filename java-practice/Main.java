12ad4b55bc18e4255bdbe58d53d80502b69dfb6d
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-09 10:21:28 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/Main.java
+++ java-practice/Main.java
@@ -56,53 +56,54 @@ public class Main {
         // }
         // Scanner sc = new Scanner(System.in);
 
-        // System.out.print("How many students? ");
-        // int count = sc.nextInt();
-        // sc.nextLine();
+        System.out.print("How many students? ");
+        int count = sc.nextInt();
+        sc.nextLine();
 
-        // // Arrays to store data
-        // int[] rollnos = new int[count];
-        // String[] names = new String[count];
-        // int[] marks = new int[count];
+        // Arrays to store data
+        int[] rollnos = new int[count];
+        String[] names = new String[count];
+        int[] marks = new int[count];
 
-        // // Input data
-        // for (int i = 0; i < count; i++) {
-        // System.out.println("\nStudent " + (i + 1) + ":");
-        // System.out.print("Enter roll number: ");
-        // rollnos[i] = sc.nextInt();
-        // sc.nextLine();
+        // Input data
+        for (int i = 0; i < count; i++) {
+            System.out.println("\nStudent " + (i + 1) + ":");
+            System.out.print("Enter roll number: ");
+            rollnos[i] = sc.nextInt();
+            sc.nextLine();
 
-        // System.out.print("Enter name: ");
-        // names[i] = sc.nextLine();
+            System.out.print("Enter name: ");
+            names[i] = sc.nextLine();
 
-        // System.out.print("Enter marks: ");
-        // marks[i] = sc.nextInt();
-        // sc.nextLine();
-        // }
-
-        // // Display table
-        // System.out.println("\n" + "=".repeat(50));
-        // System.out.printf("| %-10s | %-20s | %-10s |\n", "Roll No", "Name", "Marks");
-        // System.out.println("=".repeat(50));
-
-        // for (int i = 0; i < count; i++) {
-        // System.out.printf("| %-10d | %-20s | %-10d |\n", rollnos[i], names[i],
-        // marks[i]);
-        // }
-        // System.out.println("=".repeat(50));
-        // sc.close();
-        System.out.println("Roll no |" + "\t Name\t\t|" + " Marks |");
-        System.out.println("-------------------------------------");
-        for (int i = 0; i < 3; i++) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter roll number:");
-            int rollno = sc.nextInt();
-            System.out.println("Enter name:");
-            String name = sc.next();
-            System.out.println("Enter marks:");
-            int marks = sc.nextInt();
-            System.out.println(rollno + "\t |" + "\t" + name + "\t\t|" + " " + marks + "   |");
-            System.out.println("-------------------------------------");
+            System.out.print("Enter marks: ");
+            marks[i] = sc.nextInt();
+            sc.nextLine();
         }
+
+        // Display table
+        System.out.println("\n" + "=".repeat(50));
+        System.out.printf("| %-10s | %-20s | %-10s |\n", "Roll No", "Name", "Marks");
+        System.out.println("=".repeat(50));
+
+        for (int i = 0; i < count; i++) {
+            System.out.printf("| %-10d | %-20s | %-10d |\n", rollnos[i], names[i],
+                    marks[i]);
+        }
+        System.out.println("=".repeat(50));
+        sc.close();
+        // System.out.println("Roll no |" + "\t Name\t\t|" + " Marks |");
+        // System.out.println("-------------------------------------");
+        // for (int i = 0; i < 3; i++) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter roll number:");
+        // int rollno = sc.nextInt();
+        // System.out.println("Enter name:");
+        // String name = sc.next();
+        // System.out.println("Enter marks:");
+        // int marks = sc.nextInt();
+        // System.out.println(rollno + "\t |" + "\t" + name + "\t\t|" + " " + marks + "
+        // |");
+        // System.out.println("-------------------------------------");
+        // }
     }
 }
