ab86e517b3176d5a2cec53417849ca375156157a
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-09 21:12:10 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- java-practice/sum.java
+++ java-practice/sum.java
@@ -2,7 +2,7 @@ import java.util.*;;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+
         int ans = sum2();
         System.out.println(ans);
 
@@ -18,14 +18,13 @@ public class Sum {
         return (a + b);
     }
 
-    }
+}
 
-    static void sum() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter the second number:");
-        int b = sc.nextInt();
-        System.out.println("The sum of numbers is " + (a + b));
-    }
+static void sum() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number:");
+    int a = sc.nextInt();
+    System.out.println("Enter the second number:");
+    int b = sc.nextInt();
+    System.out.println("The sum of numbers is " + (a + b));
 }
