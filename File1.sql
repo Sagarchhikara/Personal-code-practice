31ea6a0cdb6eec970b30e68fe09aea2fe964689b
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-12 11:32:00 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- File1.sql
+++ File1.sql
@@ -187,3 +187,12 @@ VALUES (
         'F',
         '555-0110'
     );
+
+CREATE TABLE product (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    brand VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
