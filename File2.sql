aadcbb92a563b46f6d4a5d0dc7eb907f86c91603
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-10-03 11:16:33 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

---
+++ File2.sql
@@ -0,0 +1,8 @@
+USE st3;
+-- create table student(id,name,gender,contact_number) use primary key,not null,check,unique contraints
+-- add 5 records
+-- create exam table(id,subject,marks,grade,exam_date) use primary key,not null,check,unique contraints
+
+CREATE DATABASE cw;
+
+use cw;
