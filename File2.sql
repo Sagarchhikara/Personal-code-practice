e838192686801d72a20d37672b9e32585b707be3
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-10-03 11:24:26 +0530

    auto: update $(date '+%Y-%m-%d %H:%M:%S')

--- File2.sql
+++ File2.sql
@@ -11,30 +11,23 @@ use cw;
 CREATE TABLE student (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(25) NOT NULL,
-    gender VARCHAR(1) check (gender IN ('M', 'F')),
+    gender VARCHAR(1) CHECK (gender IN ('M', 'F')),
     contact_number VARCHAR(15) UNIQUE NOT NULL
 );
 
-INSERT INTO
-    student (name, gender, contact_number)
-VALUES ('John Doe', 'M', '1234567890'),
-    (
-        'Jane Smith',
-        'F',
-        '0987654321'
-    ),
-    (
-        'Alice Johnson',
-        'F',
-        '5555555555'
-    ),
-    (
-        'Bob Brown',
-        'M',
-        '4444444444'
-    ),
-    (
-        'Charlie Davis',
-        'M',
-        '3333333333'
-    );
+INSERT INTO student (name, gender, contact_number)
+VALUES
+    ('John Doe', 'M', '1234567890'),
+    ('Jane Smith', 'F', '0987654321'),
+    ('Alice Johnson', 'F', '5555555555'),
+    ('Bob Brown', 'M', '4444444444'),
+    ('Charlie Davis', 'M', '3333333333');
+
+CREATE TABLE exam (
+    id INT,
+    subject VARCHAR(50) NOT NULL,
+    marks INT CHECK (marks BETWEEN 0 AND 100),  
+    grade CHAR(1) CHECK (grade IN ('A', 'B', 'C', 'D', 'F')),
+    exam_date DATE NOT NULL,
+    CONSTRAINT fk_student FOREIGN KEY (id) REFERENCES student(id)
+);
