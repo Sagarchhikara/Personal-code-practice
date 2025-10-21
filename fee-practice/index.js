39b389c70975b73a86e31f59d0adf0a54ea95591
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-10-21 09:31:21 +0530

    Update index.js

--- fee-practice/index.js
+++ fee-practice/index.js
@@ -1,5 +1,19 @@
 // document.getElementById("count-el").innerText = 5
-let count = 0;
-console.log(myage)
-console.log(count)
-let myage = 20
+// let count = 0;
+// console.log(myage)
+// console.log(count)
+// let myage = 20
+// countdown()
+// function countdown(){
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+// countdown()
+
+// function myfunction(){
+//     console.log(42)
+// }
+// myfunction()
