a34437faeed80160904b7b09e3bdb12f577cab88
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-29 11:58:08 +0530

    auto: update 2026-01-29 11:58:08

--- backend-practice/file.js
+++ backend-practice/file.js
@@ -26,5 +26,5 @@ fs.appendFile('text.txt', '\nThis text is appended using appendFile method', (er
     if (err) console.log(err);
     else console.log("File appended successfully using appendFile method");
 });
-fs.unlinkSync('example.txt'); // unlinkSync is a method that deletes a file synchronously.
-fs.unlink("text.txt", (err) => { }) // unlink is async method that deteles a file 
+// fs.unlinkSync('example.txt'); // unlinkSync is a method that deletes a file synchronously.
+// fs.unlink("text.txt", (err) => { }) // unlink is async method that deteles a file 
