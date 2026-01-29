f4f87c74146debd5ef369378387eec938860fc1d
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-29 11:25:07 +0530

    auto: update 2026-01-29 11:25:07

--- backend-practice/file.js
+++ backend-practice/file.js
@@ -1,2 +1,4 @@
 const fs = require('fs');// require is used to import modules in Node.js
 // fs module provides an API for interacting with the file system in a manner closely modeled around standard POSIX functions.
+fs.writeFileSync('example.txt', 'Hello, World!');
+// writeFileSync is a method that writes data to a file synchronously. If the file does not exist, it will be created.
