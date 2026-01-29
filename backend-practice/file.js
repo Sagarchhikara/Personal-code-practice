4d7fc41151c2dca2dc964ba0490a436dc8c56e5b
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-29 11:28:20 +0530

    auto: update 2026-01-29 11:28:20

--- backend-practice/file.js
+++ backend-practice/file.js
@@ -3,7 +3,8 @@ const fs = require('fs');// require is used to import modules in Node.js
 fs.writeFileSync('example.txt', 'Hello, World!');
 // writeFileSync is a method that writes data to a file synchronously. If the file does not exist, it will be created.
 
-const data = fs.readFileSync('example.txt', 'utf8');
-// readFileSync is a method that reads the contents of a file synchronously. The second argument specifies the encoding.
+fs.writeFile("text.txt", "This is an example of writeFile method", (err) => {
+    const data = fs.readFileSync('example.txt', 'utf8');
+    // readFileSync is a method that reads the contents of a file synchronously. The second argument specifies the encoding.
 
-console.log(data); // Output: Hello, World!
+    console.log(data); // Output: Hello, World!
