649b5c7f3fdb1fd408e62d4f6fad4e16112a3af2
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-29 11:21:32 +0530

    auto: update 2026-01-29 11:21:32

---
+++ backend-practice/file.js
@@ -0,0 +1,2 @@
+const fs = require('fs');// require is used to import modules in Node.js
+// fs module provides an API for interacting with the file system in a manner closely modeled around standard POSIX functions.
