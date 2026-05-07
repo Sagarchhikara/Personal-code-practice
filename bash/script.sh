ce80c90fa77edb5758766a7f1e0dd0ba4efa0614
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-05-07 14:35:15 +0530

    auto: update 2026-05-07 14:35:15

--- bash/script.sh
+++ bash/script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-my_array=("apple" "banana" "cherry")
-files=(
+my_array=("apple" "banana" "cherry") # type 1 of array define 
+files=(                              # type 2 of array define
     "/etc/hosts"
     "/etc/passwd"
     "/var/log/syslog"
 )
+#type 3 of array define 
 subject[0]="Linux"
 subject[1]="Python"
 subject[2]="Dsa"
+subject[10]="BE"
 echo "${my_array[@]}"
-echo "${subject[@]}"
+echo "${subject[3]}"
 # @ means all indexs will be printed 
 
