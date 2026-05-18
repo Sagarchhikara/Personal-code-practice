1bdf59085f59b2f0d6b37d274a15615a6a912fac
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-05-18 16:46:46 +0530

    auto: update 2026-05-18 16:46:46

--- bash/script1.sh
+++ bash/script1.sh
@@ -6,3 +6,9 @@ echo "Your path is $PATH"
 echo "Your home is $HOME"
 echo "Your shell is $SHELL"
 echo "Your user is $USER"
+# defining a local variable
+myfunc(){
+	local name="sagar"
+	echo "My name is $name"
+}
+myfunc
