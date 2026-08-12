9635e462b87648e1e8941d88a7131bbbecc1c70e
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-08-12 10:41:39 +0530

    auto: update 2026-08-12 10:41:39

--- sql/pauj1.sql
+++ sql/pauj1.sql
@@ -12,3 +12,8 @@ create table student (
 );
 
 select * from student;
+
+insert into student (id, name, age, email) values (2, 'Jane Smith', 22, 'jane.smith@example.com' );
+select * from student;
+
+insert into student (id, name, age, email) values (3, 'Alice Johnson', 21, 'alice.johnson@example.com');
